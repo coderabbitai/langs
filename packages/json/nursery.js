91baf4cb66b7b77214e40d0ae67e288a7dd98a0d
Nathan Gendron <nato@coderabbit.ai>
2025-04-02 15:08:51 -0400

    ✅ Add a test to JSON

--- packages/json/nursery.js
+++ packages/json/nursery.js
@@ -1,4 +1,5 @@
 const { setup } = require('@ast-grep/nursery')
+const assert = require('node:assert')
 const languageRegistration = require('./index')
 
 setup({
@@ -7,6 +8,9 @@ setup({
   treeSitterPackage: 'tree-sitter-json',
   languageRegistration,
   testRunner: parse => {
-    // add test here
+    const sg = parse('{"abc": 123}')
+    const root = sg.root()
+    const node = root.find('{ $$$ }')
+    assert.equal(node.kind(), 'object')
   },
 })
