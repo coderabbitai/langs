f2db337744ffb5fd7d5c1c7213f3e6d1ba6dd2c7
Nathan Gendron <nato@coderabbit.ai>
2025-04-02 14:18:50 -0400

    ✅ Add test for Haskell

--- packages/haskell/nursery.js
+++ packages/haskell/nursery.js
@@ -1,4 +1,5 @@
 const { setup } = require('@ast-grep/nursery')
+const assert = require('node:assert')
 const languageRegistration = require('./index')
 
 setup({
@@ -7,6 +8,9 @@ setup({
   treeSitterPackage: 'tree-sitter-haskell',
   languageRegistration,
   testRunner: parse => {
-    // add test here
+    const sg = parse('let x = 3 in x + x')
+    const root = sg.root()
+    const node = root.find('let $A = $B in $A + $A')
+    assert.equal(node.kind(), 'let_in')
   },
 })
