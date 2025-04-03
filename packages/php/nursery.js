a7f14d638002d08b7198fdc99152b4114880be85
Nathan Gendron <nato@coderabbit.ai>
2025-04-03 16:22:19 -0400

    🚧 Use the new `src` config

--- packages/php/nursery.js
+++ packages/php/nursery.js
@@ -1,10 +1,12 @@
 const { setup } = require('@ast-grep/nursery')
 const languageRegistration = require('./index')
+const path = require('node:path')
 
 setup({
   dirname: __dirname,
   name: 'php',
   treeSitterPackage: 'tree-sitter-php',
+  src: path.join('php', 'src'),
   languageRegistration,
   testRunner: parse => {
     // add test here
