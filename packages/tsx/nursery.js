97d09187af9351d072cac6d23ec58752169653f4
Nathan Gendron <nato@coderabbit.ai>
2025-04-10 15:01:41 -0400

    🐛 path.join

--- packages/tsx/nursery.js
+++ packages/tsx/nursery.js
@@ -1,12 +1,13 @@
 const { setup } = require('@ast-grep/nursery')
-const languageRegistration = require('./index')
 const assert = require('node:assert')
+const languageRegistration = require('./index')
+const path = require('node:path')
 
 setup({
   dirname: __dirname,
   name: 'tsx',
   treeSitterPackage: 'tree-sitter-typescript',
-  src: 'tsx/src',
+  src: path.join('tsx', 'src'),
   languageRegistration,
   testRunner: parse => {
     const sg = parse('<Button onClick={() => foo()}>Click me</Button>')
