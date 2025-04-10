d612c9e9361bcff8eb619f17078a78c5fa36cecc
Nathan Gendron <nato@coderabbit.ai>
2025-04-10 14:04:11 -0400

    🐛 Set tree-sitter-typescript inner path

--- packages/typescript/nursery.js
+++ packages/typescript/nursery.js
@@ -1,10 +1,12 @@
 const { setup } = require('@ast-grep/nursery')
 const languageRegistration = require('./index')
+const path = require('node:path')
 
 setup({
   dirname: __dirname,
   name: 'typescript',
   treeSitterPackage: 'tree-sitter-typescript',
+  src: path.join('typescript', 'src'),
   languageRegistration,
   testRunner: parse => {
     // add test here
