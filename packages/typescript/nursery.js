cf2d90c671a8de144996ed5fae2b74818496c388
Nathan Gendron <nato@coderabbit.ai>
2025-04-10 14:27:20 -0400

    ✅ Add test for TypeScript

--- packages/typescript/nursery.js
+++ packages/typescript/nursery.js
@@ -1,4 +1,5 @@
 const { setup } = require('@ast-grep/nursery')
+const assert = require('node:assert')
 const languageRegistration = require('./index')
 const path = require('node:path')
 
@@ -9,6 +10,9 @@ setup({
   src: path.join('typescript', 'src'),
   languageRegistration,
   testRunner: parse => {
-    // add test here
+    const sg = parse('let x: number = 123')
+    const root = sg.root()
+    const node = root.find('let $VAR: number = $VALUE')
+    assert.equal(node.kind(), 'lexical_declaration')
   },
 })
