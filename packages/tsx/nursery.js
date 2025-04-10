97dbf2307cae84b8b1149b958a9211c369f9b1e8
Nathan Gendron <nato@coderabbit.ai>
2025-04-10 14:57:49 -0400

    ✅ Add test to tsx

--- packages/tsx/nursery.js
+++ packages/tsx/nursery.js
@@ -1,12 +1,17 @@
 const { setup } = require('@ast-grep/nursery')
 const languageRegistration = require('./index')
+const assert = require('node:assert')
 
 setup({
   dirname: __dirname,
   name: 'tsx',
-  treeSitterPackage: 'tree-sitter-tsx',
+  treeSitterPackage: 'tree-sitter-typescript',
+  src: 'tsx/src',
   languageRegistration,
   testRunner: parse => {
-    // add test here
+    const sg = parse('<Button onClick={() => foo()}>Click me</Button>')
+    const root = sg.root()
+    const node = root.find('<$ELEMENT onClick={$HANDLER}>$$$</$ELEMENT>')
+    assert.equal(node.kind(), 'jsx_element')
   },
 })
