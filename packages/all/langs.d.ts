8d96346253f738fab1ca584c42205a2f34090a6f
Nathan Gendron <nato@coderabbit.ai>
2025-04-25 15:14:59 -0400

    ♻️ Convert to TypeScript

--- packages/all/langs.d.ts
+++
@@ -1,9 +0,0 @@
-import type { DynamicLangRegistrations } from '@ast-grep/napi'
-import type { Lang } from './lang.js'
-
-/**
- * Built-in dynamic languages supported by `@ast-grep/langs`.
- */
-export declare const langs: Readonly<
-  Record<Lang, DynamicLangRegistrations[string]>
->
